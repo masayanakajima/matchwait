62416ae23a705beba851e678670c17618e761504
masayanakajima <nakajima1812@gmail.com>
2020-04-25 11:11:47 +0900

    echo localhost:8080/hello

--- main.go
+++ main.go
@@ -1,18 +1,25 @@
 package main
 
 import(
+  /*
   "log"
   "net/http"
   "path/filepath"
   "sync"
   "text/template"
+  */
+  "github.com/labstack/echo"
+  "github.com/labstack/echo/middleware"
+  "./handler"
 )
 
+/*
 type templateHandler struct {
   once sync.Once
   filename string
   templ *template.Template
 }
+
 
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
   t.once.Do(func() {
@@ -20,11 +27,21 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
   })
   t.templ.Execute(w, nil)
 }
+*/
 
 func main() {
+  e := echo.New()
+
+  e.Use(middleware.Logger())
+  e.Use(middleware.Recover())
+
+  e.GET("/hello", handler.MainPage())
+  e.Start(":8080")
+  /*
   http.Handle("/", &templateHandler{filename: "chat.html"})
   err := http.ListenAndServe(":8080", nil)
   if err != nil {
     log.Fatal("ListenAndServe", err)
   }
+  */
 }
